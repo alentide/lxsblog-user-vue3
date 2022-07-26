13b2d7602f0f8305d7bf8fc059ea68fec358aa22
alentide <3071644646@qq.com>
2022-07-26 14:30:42 +0800

    chore: 引入logo

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,19 +1,23 @@
 <template>
   <a-layout-header :style="{ position: 'fixed', zIndex: 1, width: '100%' }">
-    <div class="logo" />
+    
     <a-menu
       v-model:selectedKeys="selectedKeys"
       theme="dark"
       mode="horizontal"
       :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">首页</a-menu-item>
+    
+      <a-menu-item key="1"><img :src="Logo" alt="" style="width: 30px;height:30px;" /></a-menu-item>
+      <!-- <a-menu-item key="1">首页</a-menu-item> -->
       <a-menu-item key="2">文章</a-menu-item>
       <a-menu-item key="3">项目</a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from '@/assets/logo.png'
+</script>
 
 <style scoped></style>
